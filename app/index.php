903eb5fff1f930a445fac1cb0fc9ce3f3b6302ca
MARIEL <marielcbs@gmail.com>
2026-05-12 11:55:07 -0300

    Create index.php

---
+++ app/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="0; url=https://www.csfba.com.br">
+    <title>Redirecionando...</title>
+</head>
+<body>
+    <p>Caso não seja redirecionado, <a href="https://www.csfba.com.br">clique aqui</a>.</p>
+</body>
+</html>
