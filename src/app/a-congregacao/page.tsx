d88b7455299ba882feebad333579080e9848ce2b
marielcbs <cap@csfba.com.br>
2026-06-15 10:25:17 -0300

    Atualiza historia das irmas franciscanas

--- src/app/a-congregacao/page.tsx
+++ src/app/a-congregacao/page.tsx
@@ -12,20 +12,18 @@ export default function CongregacaoPage() {
           <div className="mt-8 space-y-6 text-lg leading-9 text-slate-700">
             <p>
               O trabalho das Irmãs Franciscanas Imaculatinas começou no século
-              passado, marcado por apóstolos como o Capuchinho Frei Ludovico
-              Acernese, nascido em 14 de abril de 1835, na Itália.
-            </p>
-            <p>
-              Em 1869, Teresa Manganiello, hoje Beata Teresa de Manganiello,
-              solicitou ingressar na Ordem Terceira, onde recebeu o nome de
-              Irmã Maria Luisa. A partir da orientação espiritual dessa jovem,
-              Frei Ludovico teve a inspiração de fundar o Instituto Irmãs
-              Franciscanas Imaculatinas em 08 de dezembro de 1881.
-            </p>
-            <p>
-              Atendendo aos objetivos do fundador, a primeira atividade
-              organizada foi uma escola primária. Depois surgiram o internato
-              para jovens e uma escola de artes feminina.
+              passado, assinalado por uma fileira de apóstolos como o
+              Capuchinho Frei Ludovico Acernese, nascido em 14 de abril de
+              1835, na Itália. Ele soube harmonizar a vida contemplativa e a
+              ação apostólica e social. Já no ano de 1869, uma jovem chamada
+              Teresa Manganiello (hoje Beata Teresa de Manganiello) solicitou
+              ingressar na Ordem Terceira, onde recebeu o nome de Irmã Maria
+              Luisa. A partir da orientação espiritual dessa jovem é que o Frei
+              Ludovico teve a inspiração de fundar o Instituto Irmãs
+              Franciscanas Imaculatinas, no dia 08 de dezembro de 1881.
+              Atendendo aos objetivos do fundador, a primeira atividade a
+              organizar foi o funcionamento de uma escola primária. Surgiu o
+              internato para jovens e uma escola de artes feminina.
             </p>
           </div>
         </div>
